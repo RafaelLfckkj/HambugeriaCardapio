752a118e514b764085a04eca5ad24d8da5d3c660
Rafael de Oliveira <rafaxolive@gmail.com>
2025-07-19 20:38:10 -0300

    fazendo o modal abir e fechar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Header } from "./components/Header"
-import { Main } from "./components/Main"
-import { Carrinho } from "./components/Carrinho"
-import { Modal } from "./components/Modal"
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Carrinho } from "./components/Carrinho";
+import { Modal } from "./components/Modal";
 
 export function App() {
+  const [modalAberto, setModalAberto] = useState(false);
+
   return (
     <div>
-      <Header/>
-      <Main/>
-      <Modal/>
-      <Carrinho/>
+      <Header />
+      <Main />
+      <Modal aberto={modalAberto} fecharModal={() => setModalAberto(false)} />
+      <Carrinho abrirModal={() => setModalAberto(true)} />
     </div>
-  )
+  );
 }
--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
-export function Carrinho() {
+export function Carrinho({ abrirModal }) {
   return (
     <footer className="w-full bg-red-500 py-3 fixed bottom-0 z-40 flex items-center justify-center cursor-pointer hover:bg-red-400 duration-300">
-      <button 
+      <button
+        onClick={abrirModal}
         className="flex items-center gap-2 text-white font-bold cursor-pointer"
         id="cart-btn"
       >
